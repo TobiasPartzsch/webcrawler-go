38069af7d034c9559010c31f74d7fee794893538
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-09-26 13:21:47 +0200

    feat: validate CLI arguments and extract base URL

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+var (
+	errNoWebsite  = errors.New("no website provided")
+	errTooManyArg = errors.New("too many arguments provided")
+)
+
+const startingCrawlMsg = `starting crawl of: %s\n`
 
 func main() {
-	fmt.Println("Hello, World!")
+	baseURL, err := validateArgs()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf(startingCrawlMsg, baseURL)
+}
+
+func validateArgs() (string, error) {
+	switch len(os.Args) {
+	case 1:
+		return "", errNoWebsite
+	case 2:
+		return os.Args[1], nil
+	default:
+		return "", errTooManyArg
+	}
 }
